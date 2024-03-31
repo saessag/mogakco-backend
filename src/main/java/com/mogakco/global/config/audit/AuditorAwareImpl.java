749e7c8750402c6b9f7a8be823c692eb5f5ae1c7
SungbinYang <ysb05222@naver.com>
2024-03-31 14:27:23 +0900

    feat: Auditing 기능 구현

---
+++ src/main/java/com/mogakco/global/config/audit/AuditorAwareImpl.java
@@ -0,0 +1,13 @@
+package com.mogakco.global.config.audit;
+
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("tester");
+    }
+}
