10ca0eaaaf97b973500fc0f4eaf5416661fa2da0
SungbinYang <ysb05222@naver.com>
2024-04-01 22:20:16 +0900

    feat: 예외 발생 시, 예외 응답 객체 및 핸들러 개발

---
+++ src/main/java/com/mogakco/global/exception/ExceptionCode.java
@@ -0,0 +1,12 @@
+package com.mogakco.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ExceptionCode {
+
+    HttpStatus getHttpStatus();
+
+    String getCode();
+
+    String getMessage();
+}
---
+++ src/main/java/com/mogakco/global/exception/custom/BusinessException.java
@@ -0,0 +1,16 @@
+package com.mogakco.global.exception.custom;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException() {
+        super();
+    }
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
