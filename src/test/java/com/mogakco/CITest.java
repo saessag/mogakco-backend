651e461b0ff95d75aaa400e15b3afc9c7740c13b
MinKyuSong <dev.isidore@gmail.com>
2024-05-01 12:30:46 +0900

    test: tc 추가

---
+++ src/test/java/com/mogakco/CITest.java
@@ -0,0 +1,15 @@
+package com.mogakco;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class CITest {
+    @Test
+    public void test() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
