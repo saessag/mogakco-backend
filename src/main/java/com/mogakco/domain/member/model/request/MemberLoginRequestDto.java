1ec3b0fd392b54a58eefbb1985b93c480189e8e1
SungbinYang <ysb05222@naver.com>
2024-06-06 19:38:13 +0900

    fix: 1차 코드리뷰 반영

--- src/main/java/com/mogakco/domain/member/model/request/MemberLoginRequestDto.java
+++ src/main/java/com/mogakco/domain/member/model/request/MemberLoginRequestDto.java
@@ -6,12 +6,12 @@ import jakarta.validation.constraints.Pattern;
 
 public record MemberLoginRequestDto(
         @NotBlank(message = "이메일은 필수 입력 값입니다.")
-        @Email(message = "유효한 이메일 주소를 입력해야 합니다.")
+        @Email(message = "이메일 혹은 비밀번호가 잘못되었습니다.")
         String email,
 
         @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
         @Pattern(regexp = "^(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,}$",
-                message = "비밀번호는 8글자이상이며 적어도 1개 이상의 특수문자를 포함해야 합니다.")
+                message = "이메일 혹은 비밀번호가 잘못되었습니다.")
         String password
 ) {
 }
